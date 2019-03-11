8390fc69d3d0f06f6e1dec9efcee74d0f33f27ef
Andrea <andrearubiolo12@gmail.com>
2019-03-11 17:11:41 +0100

    Exercise 3

---
+++ Ex3.py
@@ -0,0 +1,27 @@
+input_msg = """Insert the number corresponding to the action you want to perform:
+1. insert a new task;
+2. remove a task;
+3. show all the tasks;
+4. close the program.
+Your choice: """
+todo = ["Study", "Eat", "Go to the gym"]  # to do list
+finish = 0
+
+while not finish:
+    choice = int(input(input_msg))
+    if choice == 1:
+        task = input("Insert new task: ")
+        todo.append(task)
+        print(task, "added\n\n")
+    elif choice == 2:
+        task = input("Insert task to be deleted: ")
+        todo.remove(task)
+        print(task, "removed\n\n")
+    elif choice == 3:
+        print("\n", todo,"\n")
+    elif choice == 4:
+        finish = 1
+    else:
+        print("Error: unrecognized command.")
+
+
