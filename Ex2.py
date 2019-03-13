2354feaf26c6749aca447f70c9e88cbdd2189fc0
Andrea Rubiolo <andrearubiolo12@gmail.com>
2019-03-13 19:06:59 +0100

    Update Ex2.py
    Modified line #7

--- Ex2.py
+++ Ex2.py
@@ -4,6 +4,6 @@ str_len = len(string)
 if str_len < 2:
     new_str = ""
 else:
-    new_str = string[0] + string[1] + string[str_len-2] + string[str_len-1]
+    new_str = string[0:2] + string[str_len-2:]
 
 print(new_str)
